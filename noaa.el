68e9da0dc0103a5f6a43d0fff8ff906d27df851b
David A. Thompson <thompdump@gmail.com>
2018-11-20 12:15:08 -0800

    Adds styles for displaying forecast
    - adds styles for viewing forecast (NOAA-DISPLAY-STYLES) and means of transitioning between styles (NOAA-NEXT-STYLE)
    
    - struct NOAA-DAY-FORECAST describes the forecast for a single day 
    
    - cache last forecast at NOAA-LAST-FORECAST
    
    - removes NOAA-FORECAST and NOAA-HANDLE-NOAA-RESULT-OLD
    
    - fixes whitespace issue with latitude/longitude message

--- noaa.el
+++ noaa.el
@@ -6,7 +6,7 @@
 ;; Keywords:
 ;; Homepage: https://github.com/thomp/noaa
 ;; URL: https://github.com/thomp/noaa
-;; Package-Requires: ((request "0.2.0") (cl-lib "0.5") (emacs "24"))
+;; Package-Requires: ((request "0.2.0") (cl-lib "0.5") (emacs "24") (dash "2.14.1"))
 
 ;;; Commentary:
 
@@ -36,6 +36,9 @@
 (defvar noaa-buffer-spec "*noaa.el*"
   "Buffer or buffer name.")
 
+(defvar noaa-display-styles '(default terse)
+  "List of symbols indicating the various manners in which forecast data can be presented. The first member of the list is the currently active style.")
+
 (defface noaa-face-date '((t (:foreground "#30c2ba")))
   "Face used for date.")
 
@@ -45,7 +48,18 @@
 (defface noaa-face-temp '((t (:foreground "#cfd400")))
   "Face used for temperature.")
 
-;; noaa-forecast (vs noaa-current)
+;; Forecast data for a single day
+(defstruct noaa-day-forecast
+  start-time
+  end-time
+  day-number
+  name
+  temp
+  short-forecast)
+
+(defvar noaa-last-forecast
+  nil
+  "A set of NOAA-DAY-FORECAST structs describing the last forecast retrieved.")
 
 ;;;###autoload
 (defun noaa ()
@@ -54,7 +68,7 @@
   (if (and (numberp noaa-latitude)
 	   (numberp noaa-longitude))
       (noaa-url-retrieve (noaa-url noaa-latitude noaa-longitude))
-      (message "To use NOAA, first set NOAA-LATITUDE and NOAA-LONGITUDE.")))
+    (message "To use NOAA, first set NOAA-LATITUDE and NOAA-LONGITUDE.")))
 
 (defun noaa-aval (alist key)
   "Utility function to retrieve value associated with key KEY in alist ALIST."
@@ -62,6 +76,48 @@
     (if pair
 	(cdr pair)
       nil)))
+
+(defun noaa-display-last-forecast ()
+  (interactive)
+  (erase-buffer)
+  (let (
+	;; LAST-DAY-NUMBER is used for aesthetics --> separate data by day
+	(last-day-number -1)
+	(day-field-width 16)
+	(temp-field-width 5)
+	(forecast-length (length noaa-last-forecast)))
+    (dotimes (index forecast-length)
+      (let ((day-forecast (elt noaa-last-forecast index)))
+	(noaa-insert-day-forecast
+	 day-forecast
+	 (and (noaa-day-forecast-day-number day-forecast)
+	      last-day-number
+	      (= (noaa-day-forecast-day-number day-forecast)
+		 last-day-number)))
+	(setq last-day-number (noaa-day-forecast-day-number day-forecast))))))
+
+(defun noaa-insert-day-forecast (noaa-day-forecast last-day-p)
+  (let ((style (first noaa-display-styles)))
+    (cond ((eq style 'terse)
+	   (unless last-day-p
+	     (insert (propertize (format "%s " (substring (noaa-day-forecast-name noaa-day-forecast) 0 2))
+				 'face 'noaa-face-date)))
+	   (insert (propertize (format "%s " (noaa-day-forecast-temp noaa-day-forecast))
+			       'face 'noaa-face-temp)))
+	  ((eq style 'default)
+	   (let ((day-field-width 16)
+		 (temp-field-width 5))
+	     ;; simple output w/some alignment
+	     (unless last-day-p
+	       (newline))
+	     (insert (propertize (format "%s" (noaa-day-forecast-name noaa-day-forecast)) 'face 'noaa-face-date))
+	     (move-to-column day-field-width t)
+	     (insert (propertize (format "% s" (noaa-day-forecast-temp noaa-day-forecast)) 'face 'noaa-face-temp))
+	     (move-to-column (+ day-field-width temp-field-width) t)
+	     (insert (propertize (format "%s" (noaa-day-forecast-short-forecast noaa-day-forecast)) 'face 'noaa-face-short-forecast))
+	     (newline)))
+	  (t
+	   (error "Unrecognized style")))))
 
 (defun noaa-handle-noaa-result (result)
   "Handle the data described by RESULT (presumably the result of an HTTP request for NOAA forecast data)."
@@ -72,30 +128,11 @@
     (let ((properties (funcall retrieve-fn result 'properties)))
       (if (not properties)
 	  (message "Couldn't find properties. The NOAA API spec may have changed.")
-	(let ((periods (funcall retrieve-fn properties 'periods))
-	      ;; LAST-DAY-NUMBER is used for aesthetics --> separate data by day
-	      (last-day-number -1)
-	      (day-field-width 16)
-	      (temp-field-width 5))
-	 (erase-buffer)
-	 (dotimes (i (length periods))
-	   (let ((period (elt periods i)))
-	     (let ((start-time (funcall retrieve-fn period 'startTime)))
-	       (let ((day-number (noaa-iso8601-to-day start-time))
-		     (name (funcall retrieve-fn period 'name))
-		     (temp (funcall retrieve-fn period 'temperature))
-		     (short-forecast (funcall retrieve-fn period 'shortForecast)))
-		 ;; simple output w/some alignment
-		 (progn (if (not (= last-day-number day-number))
-			    (newline))
-			(insert (propertize (format "%s" name) 'face 'noaa-face-date))
-			(move-to-column day-field-width t)
-			(insert (propertize (format "% s" temp) 'face 'noaa-face-temp))
-			(move-to-column (+ day-field-width temp-field-width) t)
-			(insert (propertize (format "%s" short-forecast) 'face 'noaa-face-short-forecast))
-			(newline))
-		 (setq last-day-number day-number)))))
-	 (goto-char (point-min)))))))
+	(let ((periods (funcall retrieve-fn properties 'periods)))
+	  (message "have periods")
+	  (noaa-set-last-forecast periods)
+	  (message "displaying last forecast")
+	  (noaa-display-last-forecast))))))
 
 ;; emacs built-ins aren't there yet for handling ISO8601 values -- leaning on date is non-portable but works nicely for systems where date is available
 (defun noaa-iso8601-to-day (iso8601-string)
@@ -107,6 +144,28 @@
   "Leave the buffer specified by ‘noaa-buffer-spec’."
   (interactive)
   (kill-buffer noaa-buffer-spec))
+
+(defun noaa-clear-last-forecast ()
+  (setf noaa-last-forecast nil))
+
+(defun noaa-set-last-forecast (periods)
+  (message "periods: %S " periods)
+  (noaa-clear-last-forecast)
+  ;; retrieve-fn accepts two arguments: a key-value store and a key
+  ;; retrieve-fn returns the corresponding value
+  (let ((retrieve-fn 'noaa-aval)
+	(number-of-periods (length periods)))
+    (setf noaa-last-forecast (make-list (length periods) nil))
+    (dotimes (i number-of-periods)
+      (let ((period (elt periods i)))
+	(let ((start-time (funcall retrieve-fn period 'startTime)))
+	  (let ((day-number (noaa-iso8601-to-day start-time))
+		;; NAME is descriptive. It is not always the name of a week day. Exaples of valid values include "This Afternoon", "Thanksgiving Day", or "Wednesday Night".
+		(name (funcall retrieve-fn period 'name))
+		(temp (funcall retrieve-fn period 'temperature))
+		(short-forecast (funcall retrieve-fn period 'shortForecast)))
+	    (setf (elt noaa-last-forecast i)
+		  (make-noaa-day-forecast :start-time start-time :end-time nil :day-number day-number :name name :temp temp :short-forecast short-forecast))))))))
 
 (defun noaa-url (&optional latitude longitude)
   "Return a string representing a URL. LATITUDE and LONGITUDE should be numbers."
@@ -159,6 +218,14 @@
   (switch-to-buffer noaa-buffer-spec)
   (insert x))
 
+(defun noaa-next-style ()
+  (interactive)
+  ;; wouldn't hurt to add this to other (interactive) fns that should only operate within noaa-mode
+  (unless (eq (current-buffer) (get-buffer noaa-buffer-spec))
+    (display-warning :warning (format "Not in %s buffer" noaa-buffer-spec)))
+  (setf noaa-display-styles (-rotate 1 noaa-display-styles)) ; dash provides -rotate
+  (noaa-display-last-forecast))
+
 ;;
 ;; noaa mode
 ;;
@@ -174,6 +241,7 @@
   "Keymap for `noaa-mode'.")
 
 (define-key noaa-mode-map (kbd "q") 'noaa-quit)
+(define-key noaa-mode-map (kbd "n") 'noaa-next-style)
 
 (provide 'noaa)
 ;;; noaa.el ends here
