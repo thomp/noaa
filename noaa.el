96efd660c494ea2ebfacdd1858acd46de3438257
David A. Thompson <thompdump@gmail.com>
2022-07-31 18:43:18 -0700

    Updates copyright date clause

--- noaa.el
+++ noaa.el
@@ -1,6 +1,6 @@
 ;;; noaa.el --- Get NOAA weather data -*- lexical-binding: t -*-
 
-;; Copyright (C) 2017,2018, 2021 David Thompson
+;; Copyright (C) 2017 - 2022 David Thompson
 ;; Author: David Thompson
 ;; Version: 0.1
 ;; Keywords: calendar
